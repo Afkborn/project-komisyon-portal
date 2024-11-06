68caaba1ba08fec4e0880e593d7bfdc059182349
Bilgehan Kalay <kalaybilgehan60@gmail.com>
2024-11-06 20:55:03 +0300

    API yönlendirmesi için hedef port numarası güncellendi; localhost'taki port 2626 olarak değiştirildi.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   const target =
-    process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/api";
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:2626/api";
   app.use(
     "/api",
     createProxyMiddleware({
